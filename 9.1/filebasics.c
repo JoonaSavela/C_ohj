f0d41d68325313f7433408723bfb52324785c791
Savela Joona <savelaj3@kiwi.org.aalto.fi>
2018-03-12 23:49:09 +0200

    9.1 trials

--- 9.1/filebasics.c
+++ 9.1/filebasics.c
@@ -18,12 +18,40 @@ int print_file(const char *filename) {
         }
     }
 
+    fclose(f);
+
     return res;
 }
 
 char *difference(const char* file1, const char* file2) {
-    char *res = NULL;
+    char *res = malloc(150 * sizeof(char));
+    FILE *f1 = fopen(file1, "r");
+    FILE *f2 = fopen(file2, "r");
 
+    char ss1[50];
+    char ss2[50];    
+    char *s1 = ss1;
+    char *s2 = ss2;
+
+    while (s1 != NULL && s2 != NULL) {
+        s1 = fgets(s1, 50, f1);
+        s2 = fgets(s2, 50, f2);
+
+        if (s1 != NULL && s2 != NULL && strcmp(s1, s2) != 0) {
+            strcpy(res, "    ");
+            
+            strcpy(res + 4, s1);
+
+            strcpy(res + 4 + strlen(s1), "----\n    ");
+
+            strcpy(res + 4 + strlen(s1) + 9, s2);
+
+            return res;
+        }
+    }
+
+    fclose(f1);
+    fclose(f2);
     return res;
 }
 
