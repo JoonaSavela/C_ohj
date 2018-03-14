f763b3383b17827dfbcaad460dbae3e85c706b8a
Savela Joona <savelaj3@kosh.org.aalto.fi>
2018-03-14 11:07:25 +0200

    9.3

--- 9.3/filestats.c
+++ 9.3/filestats.c
@@ -1,23 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "filestats.h"
 
 int line_count(const char *filename) {
     FILE *f = fopen(filename, "r");
     if (!f) return -1;
     int count = 0;
+    int flag_newline = 1;
+    char *p = malloc(sizeof(char));
+    size_t n = fread(p, sizeof(char), 1, f);
 
+    while (n == 1) {
+        if (flag_newline) {
+            count++;
+            flag_newline = 0;
+        }
+        if (*p == '\n') {
+            flag_newline = 1;
+        }
+        n = fread(p, sizeof(char), 1, f);
+    }
 
-
+    free(p);
     fclose(f);
     return count;
 }
 
 int word_count(const char *filename) {
     FILE *f = fopen(filename, "r");
-    if (f!) return -1;
+    if (!f) return -1;
     int count = 0;
+    int flag_space = 1;
+    char *p = malloc(sizeof(char));
+    size_t n = fread(p, sizeof(char), 1, f);
 
+    while (n == 1) {
+        if (isalpha(*p) && flag_space) {
+            count++;
+            flag_space = 0;
+        }
+        if (isspace(*p)) {
+            flag_space = 1;
+        }
+        n = fread(p, sizeof(char), 1, f);
+    }
 
+    free(p);
     fclose(f);
     return count;
 }
